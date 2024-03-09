a303abf510f517d42646971eddc7f4075a9d5d16
devPisa <cesar.au563@gmail.com>
2024-03-09 16:33:45 -0300

    Conversor funcionando

---
+++ src/main/java/com/conversor/temperatura/ConversorController/ConversorController.java
@@ -0,0 +1,34 @@
+package com.conversor.temperatura.ConversorController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ConversorController {
+    @GetMapping("/")
+    public ModelAndView showConversorForm(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("Conversor");
+        return modelAndView;
+    }
+    @PostMapping("calculate")
+    public String calculate (@RequestParam("temp") double temp,
+                             @RequestParam("select") String select,
+                            Model model) {
+     double result = 0;
+     switch (select){
+         case "Fahrenheit":
+             result = (temp * (9.0/5.0) + 32.0);
+             break;
+         case "Kelvin":
+             result = (temp + 273.15);
+             break;
+     }
+     model.addAttribute("result", result);
+     return "Conversor";
+    }
+}
